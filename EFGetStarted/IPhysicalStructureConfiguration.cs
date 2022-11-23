ca46407d59dea081d82f3eeca9c6b8be9556536d
Sturla Þorvaldsson <sturla.thorvaldsson@marel.com>
2022-11-23 08:17:53 +0000

    Add project files.

---
+++ EFGetStarted/IPhysicalStructureConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace EFGetStarted;
+
+/// <summary>
+/// Interface defining configurations for Physicial Structure service.
+/// </summary>
+public interface IPhysicalStructureConfiguration
+{
+    /// <summary>
+    /// Connection string to database.
+    /// </summary>
+    string? ConnectionString { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the service is configured for cloud.
+    /// </summary>
+    bool CloudEnabled { get; set; }
+}
---
+++ EFGetStarted/PhysicalStructureConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EFGetStarted;
+
+namespace PhysicalStructureService.Configurations;
+
+/// <summary>
+/// Configuration class for Physicial Structure service.
+/// </summary>
+public class PhysicalStructureConfiguration : IPhysicalStructureConfiguration
+{
+    /// <summary>
+    /// Connection string to database.
+    /// </summary>
+    public string? ConnectionString { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the service is configured for cloud.
+    /// </summary>
+    public bool CloudEnabled { get; set; }
+}
